fb4f62bf6c113e4c31d9802f1d6bf07465326b60
Gauthier Bastien <g.bastien@imio.be>
2014-05-27 14:40:09 +0000

    Do tests pass

--- src/Products/MeetingLiege/tests/testMeetingFileType.py
+++ src/Products/MeetingLiege/tests/testMeetingFileType.py
@@ -22,11 +22,11 @@
 # 02110-1301, USA.
 #
 
-from Products.MeetingLalouviere.tests.MeetingLalouviereTestCase import MeetingLalouviereTestCase
+from Products.MeetingLiege.tests.MeetingLiegeTestCase import MeetingLiegeTestCase
 from Products.MeetingCommunes.tests.testMeetingFileType import testMeetingFileType as mctmft
 
 
-class testMeetingFileType(MeetingLalouviereTestCase, mctmft):
+class testMeetingFileType(MeetingLiegeTestCase, mctmft):
     '''Tests the MeetingFileType class methods.'''
 
 

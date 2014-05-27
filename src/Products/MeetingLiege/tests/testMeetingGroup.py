33ceb551d3b9cc8befb1a243b38d616cebaeb3ef
Gauthier Bastien <g.bastien@imio.be>
2014-05-27 14:41:21 +0000

    Do tests pass

--- src/Products/MeetingLiege/tests/testMeetingGroup.py
+++ src/Products/MeetingLiege/tests/testMeetingGroup.py
@@ -22,11 +22,11 @@
 # 02110-1301, USA.
 #
 
-from Products.MeetingLalouviere.tests.MeetingLalouviereTestCase import MeetingLalouviereTestCase
+from Products.MeetingLiege.tests.MeetingLiegeTestCase import MeetingLiegeTestCase
 from Products.MeetingCommunes.tests.testMeetingGroup import testMeetingGroup as mctmg
 
 
-class testMeetingGroup(MeetingLalouviereTestCase, mctmg):
+class testMeetingGroup(MeetingLiegeTestCase, mctmg):
     '''Tests the testMeetingGroup class methods.'''
 
 

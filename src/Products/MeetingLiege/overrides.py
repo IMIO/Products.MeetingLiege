bf6c135bccf986314dc7c43d7e390bac64e10864
Gauthier Bastien <g.bastien@imio.be>
2015-09-28 09:53:17 +0000

    Adapted override of 'Products.PloneMeeting.content.advice.advice_group_vocabulary' to take original vocabulary values into account so we benefit from checks made in original vocabulary, see ticket #12002

--- src/Products/MeetingLiege/overrides.py
+++ src/Products/MeetingLiege/overrides.py
@@ -1,12 +1,13 @@
 from zope.component import queryUtility
 from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleTerm, SimpleVocabulary
+from zope.schema.vocabulary import SimpleVocabulary
 from Products.CMFCore.utils import getToolByName
 from imio.history.adapters import ImioWfHistoryAdapter
 from imio.history.utils import getPreviousEvent
 from Products.PloneMeeting.adapters import AnnexableAdapter
 from Products.PloneMeeting.adapters import PMWfHistoryAdapter
+from Products.PloneMeeting.content.advice import AdviceTypeVocabulary as PMAdviceTypeVocabulary
 from Products.MeetingLiege.config import FINANCE_GROUP_IDS
 
 
@@ -20,34 +21,27 @@ class AdviceTypeVocabulary(object):
         cfg = tool.getMeetingConfig(context)
         if not cfg:
             return SimpleVocabulary(terms)
-        usedAdviceTypes = cfg.getUsedAdviceTypes()
-        # make sure if an adviceType was used for context and it is no more available, it
-        # appears in the vocabulary and is so useable...
-        if context.portal_type == 'meetingadvice' and not context.advice_type in usedAdviceTypes:
-            usedAdviceTypes = usedAdviceTypes + (context.advice_type, )
+
+        # call original vocabulary method in PM that does different checks
+        # and remove relevant values depending on fact that it is a finance group or not
+        original_res = PMAdviceTypeVocabulary()(context)
+
         # if finance group in group vocabulary, we use special _finance advice types
         factory = queryUtility(IVocabularyFactory, u'Products.PloneMeeting.content.advice.advice_group_vocabulary')
-        # self.context is portal
         groupVocab = factory(context)
         groupIds = set([group.value for group in groupVocab._terms])
         if set(FINANCE_GROUP_IDS).intersection(groupIds):
             # we are on a finance group, only keep _finance advice types
-            res = list(usedAdviceTypes)
-            for adviceType in usedAdviceTypes:
-                if not adviceType in ('positive_finance', 'negative_finance', 'not_required_finance', ):
-                    res.remove(adviceType)
-            usedAdviceTypes = tuple(res)
+            terms = []
+            for term in original_res:
+                if term.token.endswith('_finance'):
+                    terms.append(term)
         else:
             # not a finance group, remove specific _finance types
-            res = list(usedAdviceTypes)
-            for adviceType in usedAdviceTypes:
-                if adviceType in ('positive_finance', 'negative_finance', 'not_required_finance', ):
-                    res.remove(adviceType)
-            usedAdviceTypes = tuple(res)
-
-        for advice_id, advice_title in cfg.listAdviceTypes().items():
-            if advice_id in usedAdviceTypes:
-                terms.append(SimpleTerm(advice_id, advice_id, advice_title))
+            terms = []
+            for term in original_res:
+                if not term.token.endswith('_finance'):
+                    terms.append(term)
         return SimpleVocabulary(terms)
 
 

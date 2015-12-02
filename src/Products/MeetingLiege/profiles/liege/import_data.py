cb13526402aee24670a136558013d3ec0084ac40
Gauthier Bastien <g.bastien@imio.be>
2015-12-02 14:18:24 +0000

    'tool' and 'cfg' are available in PodTemplates TALCondition expressions

--- src/Products/MeetingLiege/profiles/liege/import_data.py
+++ src/Products/MeetingLiege/profiles/liege/import_data.py
@@ -35,24 +35,24 @@ annexeAvisLegal = MeetingFileTypeDescriptor('annexeAvisLegal', 'Extrait article 
 agendaTemplate = PodTemplateDescriptor('oj', 'Ordre du jour')
 agendaTemplate.odt_file = 'college-oj.odt'
 agendaTemplate.pod_portal_types = ['MeetingCollege']
-agendaTemplate.tal_condition = 'python: here.portal_plonemeeting.isManager(here)'
+agendaTemplate.tal_condition = 'python: tool.isManager(here)'
 
 agendaTemplatePDF = PodTemplateDescriptor('oj-pdf', 'Ordre du jour')
 agendaTemplatePDF.odt_file = 'college-oj.odt'
 agendaTemplatePDF.pod_formats = ['pdf', ]
 agendaTemplatePDF.pod_portal_types = ['MeetingCollege']
-agendaTemplatePDF.tal_condition = 'python: here.portal_plonemeeting.isManager(here)'
+agendaTemplatePDF.tal_condition = 'python: tool.isManager(here)'
 
 decisionsTemplate = PodTemplateDescriptor('pv', 'Procès-verbal')
 decisionsTemplate.odt_file = 'college-pv.odt'
 decisionsTemplate.pod_portal_types = ['MeetingCollege']
-decisionsTemplate.tal_condition = 'python: here.portal_plonemeeting.isManager(here)'
+decisionsTemplate.tal_condition = 'python: tool.isManager(here)'
 
 decisionsTemplatePDF = PodTemplateDescriptor('pv-pdf', 'Procès-verbal')
 decisionsTemplatePDF.odt_file = 'college-pv.odt'
 decisionsTemplatePDF.pod_formats = ['pdf', ]
 decisionsTemplatePDF.pod_portal_types = ['MeetingCollege']
-decisionsTemplatePDF.tal_condition = 'python: here.portal_plonemeeting.isManager(here)'
+decisionsTemplatePDF.tal_condition = 'python: tool.isManager(here)'
 
 itemProjectTemplate = PodTemplateDescriptor('projet-deliberation', 'Projet délibération')
 itemProjectTemplate.odt_file = 'projet-deliberation.odt'

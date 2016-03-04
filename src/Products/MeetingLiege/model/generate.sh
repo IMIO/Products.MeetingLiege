f7d2d27a3fbc33867013313ad159df5c1d4a7f62
Gauthier Bastien <g.bastien@imio.be>
2016-03-04 08:44:03 +0000

    Use archgenxml-master like before

--- src/Products/MeetingLiege/model/generate.sh
+++ src/Products/MeetingLiege/model/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/srv/archgenxml/bin/archgenxml --cfg generate.conf MeetingLiege.zargo -o tmp
+archgenxml-master --cfg generate.conf MeetingLiege.zargo -o tmp 
 
 # only keep workflows
 cp -rf tmp/profiles/default/workflows ../profiles/default

6b034c3b9dc1611c9eea156f0d33976af97322d6
Gauthier Bastien <g.bastien@imio.be>
2016-09-26 13:28:13 +0000

    Override MeetingItem.showAdvices test

--- src/Products/MeetingLiege/tests/testAdvices.py
+++ src/Products/MeetingLiege/tests/testAdvices.py
@@ -29,6 +29,17 @@ from Products.MeetingLiege.tests.MeetingLiegeTestCase import MeetingLiegeTestCas
 class testAdvices(MeetingLiegeTestCase, pmta):
     '''Call testAdvices from PloneMeeting.'''
 
+    def test_pm_ShowAdvices(self):
+        """Always True."""
+        self.changeUser('pmCreator1')
+        item = self.create('MeetingItem')
+        self.assertEqual(self.tool.getMeetingConfig(item), self.meetingConfig)
+        self.assertTrue(item.adapted().showAdvices())
+        self.setMeetingConfig(self.meetingConfig2.getId())
+        item2 = self.create('MeetingItem')
+        self.assertEqual(self.tool.getMeetingConfig(item2), self.meetingConfig2)
+        self.assertTrue(item2.adapted().showAdvices())
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite

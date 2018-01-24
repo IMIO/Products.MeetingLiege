48e69f1b22e8aefe365f48865637681b5aa6cbd8
Gauthier Bastien <g.bastien@imio.be>
2018-01-24 08:30:09 +0000

    Make WF adaptation 'return_to_proposing_group' useable for BG config

--- src/Products/MeetingLiege/adapters.py
+++ src/Products/MeetingLiege/adapters.py
@@ -99,7 +99,9 @@ MeetingConfig.wfAdaptations = customWfAdaptations
 RETURN_TO_PROPOSING_GROUP_STATE_TO_CLONE = {'meetingitemcollegeliege_workflow':
                                             'meetingitemcollegeliege_workflow.itemcreated',
                                             'meetingitemcouncilliege_workflow':
-                                            'meetingitemcollegeliege_workflow.itemcreated', }
+                                            'meetingitemcouncilliege_workflow.itemcreated',
+                                            'meetingitembourgmestre_workflow':
+                                            'meetingitembourgmestre_workflow.itemcreated'}
 adaptations.RETURN_TO_PROPOSING_GROUP_STATE_TO_CLONE = RETURN_TO_PROPOSING_GROUP_STATE_TO_CLONE
 
 

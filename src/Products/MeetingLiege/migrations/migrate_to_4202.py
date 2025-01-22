e2e6f978b973ebe99e0595e9eaa754120db97357
Gauthier Bastien <gauthier.bastien@imio.be>
2025-01-22 16:47:36 +0100

    Completed migration step to MeetingLiege 4202

--- src/Products/MeetingLiege/migrations/migrate_to_4202.py
+++ src/Products/MeetingLiege/migrations/migrate_to_4202.py
@@ -20,12 +20,20 @@ class Migrate_To_4202(Migrator):
         for cfg in self.tool.objectValues('MeetingConfig'):
             archivingRefs = getattr(cfg, 'archivingRefs', ())
             if archivingRefs:
+                # enable classifier
+                self.update_used_attrs(
+                    to_add=['classifier'],
+                    to_remove=['archivingRef'],
+                    cfg_ids=[cfg.getId()])
+                # enable classifier faceted filter everywhere
+                self.update_faceted_filters(to_add=['c24'], cfg_ids=[cfg.getId()])
                 # create classifiers in MeetingConfig
                 for archivingRef in archivingRefs:
                     api.content.create(container=cfg.classifiers,
-                                       type="meetintcategory",
+                                       type="meetingcategory",
                                        id=archivingRef['row_id'],
-                                       title="%s %s".format(archivingRef['code'], archivingRef['label']),
+                                       title=archivingRef['label'],
+                                       category_id=archivingRef['code'],
                                        using_groups=archivingRef['restrict_to_groups'],
                                        enabled=True if archivingRef['active'] == '1' else False)
                 self.cleanMeetingConfigs(field_names=['archivingRefs'])
@@ -40,7 +48,7 @@ class Migrate_To_4202(Migrator):
                     item = brain.getObject()
                     item.setClassifier(item.archivingRef)
                     delattr(item, 'archivingRef')
-                    item.reindexObject(idxs=['getRawClassifier'], update_metadata=True)
+                    item.reindexObject(idxs=['getRawClassifier'])
         logger.info('Done.')
 
     def run(self,

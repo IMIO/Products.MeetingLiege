b1b7afcd80daf3cbf637cabe7c3b0c6685245a29
Gauthier Bastien <gauthier.bastien@imio.be>
2025-03-17 14:58:06 +0100

    Overrided test_pm_deliberation_for_restapi

--- src/Products/MeetingLiege/tests/testViews.py
+++ src/Products/MeetingLiege/tests/testViews.py
@@ -12,6 +12,45 @@ from Products.PloneMeeting.tests.testViews import testViews as pmtv
 class testViews(MeetingLiegeTestCase, pmtv):
     ''' '''
 
+    def test_pm_deliberation_for_restapi(self):
+        """Override and print_deliberation was overrided."""
+        self.changeUser('pmCreator1')
+        item = self.create('MeetingItem', motivation=self.motivationText, decision=self.decisionText)
+        view = item.restrictedTraverse('@@document-generation')
+        helper = view.get_generation_context_helper()
+        data = helper.deliberation_for_restapi()
+        self.assertEqual(
+            data["deliberation"],
+            self.motivationText +
+            "<p>Sur proposition du Coll&#232;ge communal, et apr&#232;s examen "
+            "du dossier par la Commission comp&#233;tente ;</p>" +
+            self.decisionText)
+        self.assertEqual(
+            data["deliberation_motivation"],
+            self.motivationText +
+            "<p>Sur proposition du Coll&#232;ge communal, et apr&#232;s examen "
+            "du dossier par la Commission comp&#233;tente ;</p>" +
+            self.decisionText)
+        self.assertEqual(
+            data["deliberation_decision"],
+            self.motivationText +
+            "<p>Sur proposition du Coll&#232;ge communal, et apr&#232;s examen "
+            "du dossier par la Commission comp&#233;tente ;</p>" +
+            self.decisionText)
+        self.assertEqual(
+            data["public_deliberation"],
+            self.motivationText +
+            "<p>Sur proposition du Coll&#232;ge communal, et apr&#232;s examen "
+            "du dossier par la Commission comp&#233;tente ;</p>" +
+            self.decisionText)
+        self.assertEqual(
+            data["public_deliberation_decided"],
+            self.motivationText +
+            "<p>Sur proposition du Coll&#232;ge communal, et apr&#232;s examen "
+            "du dossier par la Commission comp&#233;tente ;</p>" +
+            self.decisionText)
+        return item, view, helper, data
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
